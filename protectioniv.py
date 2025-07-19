147366eb9c25dc004f11bc49a8796ebc2aabf516
ambatabuss <white337890@gmail.com>
2025-07-19 03:55:12 +0300

    Update protectioniv.py

--- protectioniv.py
+++ protectioniv.py
@@ -4,6 +4,17 @@ import re
 from collections import defaultdict
 import asyncio
 import os
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run_flask():
+    app.run(host='0.0.0.0', port=8080)
 
 
 
